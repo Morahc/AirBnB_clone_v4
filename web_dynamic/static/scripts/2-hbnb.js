ce02eb6e81fcd1674fa26cf0e8cdaa843ff4f667
Morah Stephen <Morahstephen872@gmail.com>
2023-05-12 18:51:23 +0100

    feature: created files

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,20 @@
+$("document").ready(function () {
+    const amenities = {};
+    $("input[type=checkbox").change(function () {
+        if (this.checked) {
+            amenities[this.dataset.name] = this.dataset.id;
+        } else {
+            delete amenities[this.dataset.name];
+        }
+        $(".amenities h4").text(Object.keys(amenities).sort().join(", "));
+    });
+    $.getJSON("http://127.0.0.1:5001/api/v1/status/", (response) =>{
+        if (response.status === "OK") {
+            $("div#api_status").addClass("available");
+            $('div#api_status').removeAttr('id');
+            
+        } else {
+            $("div#api_status").removeClass("available");
+        }
+    })
+});
